23fec6f6a09dc7e598786c1f31043753770a5be6
Benjamin Herber <benjiherbs@gmail.com>
2025-03-01 17:26:38 -0500

    add properties file

--- tools/server-jar/src/main/java/org/eclipse/rdf4j/server/Application.java
+++ tools/server-jar/src/main/java/org/eclipse/rdf4j/server/Application.java
@@ -2,10 +2,16 @@ package org.eclipse.rdf4j.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(
+        value = "classpath:org/eclipse/rdf4j/http/server/application.properties",
+        ignoreResourceNotFound = true,
+        name = "org.springframework.context.support.PropertySourcesPlaceholderConfigurer"
+)
 public class Application {
-	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
