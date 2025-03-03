b42cbd98d6f16e77d54fe74f9715d0439bd4d026
Benjamin Herber <benjiherbs@gmail.com>
2025-03-02 20:45:50 -0500

    mark as experimental

--- tools/server-jar/src/main/java/org/eclipse/rdf4j/server/Application.java
+++ tools/server-jar/src/main/java/org/eclipse/rdf4j/server/Application.java
@@ -1,9 +1,11 @@
 package org.eclipse.rdf4j.server;
 
+import org.eclipse.rdf4j.common.annotation.Experimental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+@Experimental
 @SpringBootApplication
 @PropertySource(
         value = "classpath:org/eclipse/rdf4j/http/server/application.properties",
